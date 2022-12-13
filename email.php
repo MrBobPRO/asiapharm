0421c7f3b58d7686a8d281302dfe5f3d287d2265
Sh <nazriev.shavqatjon@gmail.com>
2022-12-13 16:30:12 +0500

    Editing form submit

--- email.php
+++ email.php
@@ -1,9 +1,15 @@
 <?php
-  $name = $_POST['name'];
-  $phone = $_POST['phone'];
+$name = $_POST['name'];
+$phone = $_POST['phone'];
 
-  if($name && $phone) {
-    echo "name: $name<br> phone: $phone";
-  } else {
-    echo "Fisk you!";
-  }
+if ($name && $phone) {
+  $to = 'boburjon_n@mail.ru';
+  $subject = 'Новое сообщение asiapharm.tj';
+  $message = "<h2>Запрос о бесплатной консультации из сайта asiapharm.tj</h2><p>Имя: $name<br>Телефон: $phone</p>";
+
+  $headers = 'From: test@test.com' . "\r\n" .
+    'Reply-To: test@test.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+  mail($to, $subject, $message, $headers);
+}
