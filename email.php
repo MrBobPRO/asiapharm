e5fdc45e0a075a26d481cdd320d2ef2d1adb35cf
Sh <nazriev.shavqatjon@gmail.com>
2022-12-14 13:07:35 +0500

    Media styles added

--- email.php
+++ email.php
@@ -4,18 +4,17 @@ $phone = $_POST['phone'];
 
 if ($name && $phone) {
   $to = 'boburjon_n@mail.ru';
-  $subject = 'Новое сообщение asiapharm.tj';
-  $message = "Запрос о бесплатной консультации из сайта asiapharm.tj. Имя: $name. Телефон: $phone";
+  $subject = 'Новое сообщение из сайта asiapharm.tj';
+  $message = "Новый запрос о бесплатной консультации. Имя: $name. Телефон: $phone";
 
   $headers = 'From: test@demo.asiapharm.tj' . "\r\n" .
     'Reply-To: test@demo.asiapharm.tj' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
-  if(mail($to, $subject, $message, $headers)) {
+  if (mail($to, $subject, $message, $headers)) {
     header("Location: http://asiapharm.tj/");
     die();
   } else {
     echo "Сервис временно не работает!";
   }
-
 }
